d59c98b1340ee258805279f3df2c1558b6508c35
David Afonso <afonso27000@gmail.com>
2023-06-11 19:44:50 +0100

    JS fundamentals

---
+++ fundamentals.js
@@ -0,0 +1,13 @@
+// Objects
+
+const factObj = {
+    text: "Lisbon is the capital of Portugal",
+    category: "Society",
+    createdIn: 2015,
+    isCorrect: true
+}
+
+console.log(factObj.text)
+console.log(factObj.category)
+console.log(factObj.createdIn)
+console.log(factObj.isCorrect)
