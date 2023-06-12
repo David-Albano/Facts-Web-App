9739772464227598f93bb0fb09503f5cda1d5fe0
David Afonso Méndez <afonso27000@gmail.com>
2023-06-12 16:49:39 +0100

    updating

--- fundamentals.js
+++ fundamentals.js
@@ -1,3 +1,39 @@
+year = 2023
+
+const msg = `We are in the year ${year}`
+console.log(msg)
+
+// Arrow Functions
+
+    // function name         arguments                     return
+const calculateYear = (year) => new Date().getFullYear() - year
+
+console.log(calculateYear(1997))
+console.log(calculateYear(2013))
+console.log(calculateYear(2007))
+
+
+// Arrays
+
+const fact = ["Lisbon is the capital of Portugal", 2015, true]
+console.log(fact)
+console.log(fact[0])
+console.log(fact[1])
+console.log(fact[2])
+console.log(fact.length)
+
+const [text, createdIn, isTrue] = fact
+
+console.log(text)
+console.log(createdIn)
+console.log(isTrue)
+
+// Unpacking arrays with ...array
+const newFact = [...fact, "Society"]
+console.log(newFact)
+const newFact2 = ["Society", ...fact]
+console.log(newFact2)
+
 // Objects
 
 const factObj = {
