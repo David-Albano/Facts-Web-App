370d7d1cc17095e2b0916c17acd125a0daf65c5d
David Afonso Méndez <afonso27000@gmail.com>
2023-06-16 16:41:48 +0100

    Finishing all the components structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
 import './style.css'
 
+const initialFacts = [
+  {
+    id: 1,
+    text: "React is being developed by Meta (formerly facebook)",
+    source: "https://opensource.fb.com/",
+    category: "technology",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+    source:
+      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+    category: "society",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: "Lisbon is the capital of Portugal",
+    source: "https://en.wikipedia.org/wiki/Lisbon",
+    category: "society",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2015,
+  },
+];
+
 function App() {
   const appTitle = "Today I Learned"
 
@@ -33,12 +67,85 @@ function NewFactForm(){
   return <form className="fact-form">Fact form</form>
 }
 
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
+
 function CategoryFilter() {
-  return <aside>Category Filter</aside>
+  return (
+    <aside>
+      <ul>
+      <li className="category">
+          <button className="btn btn-all-categories">All</button>
+      </li>
+
+      {CATEGORIES.map((category) => (
+        <li key={category.name} className="category">
+            <button className="btn btn-category" style={{ backgroundColor: category.color}}>
+              {category.name}
+            </button>
+        </li>
+      ))}
+      </ul>
+    </aside>
+  )
 }
 
 function FactList(){
-  return <section>Fact List</section>
+  const facts = initialFacts
+
+  return (
+    <section>
+      <ul className="facts-list">
+        {facts.map((fact) => (
+          <Fact key={fact.id} fact={fact} />
+        ))}
+      </ul>
+      <p>There are {facts.length} facts in the database. Add your own!</p>
+    </section>
+  )
+}
+
+// const {factObj} = props   // (const factObj = props.factObj)
+function Fact({ fact }) {
+    return(
+      <li className="fact">
+        <p>
+            { fact.text }
+            <a className="source"
+                href={fact.source} 
+                target="_blank">
+                (Source)
+            </a>
+        </p>
+        {/* Is needed JavaScript    {{   property    :   value }} */}
+        <span className="tag" style=
+            {{ backgroundColor: CATEGORIES.find(
+                (category) =>
+                category.name === fact.category).color
+            }}>
+            {fact.category}
+        </span>
+        <div className="vote-buttons">
+            <button>
+                <div className="thumb-up">👍</div>
+                <div> {fact.votesInteresting}</div></button>
+            <button>
+                <div className="mind-blowing">🤯</div>
+                <div> {fact.votesMindblowing}</div></button>
+            <button>
+                <div className="forbidden">⛔️</div>
+                <div> {fact.votesFalse}</div></button>
+        </div>
+      </li>
+    )
 }
 
 export default App
